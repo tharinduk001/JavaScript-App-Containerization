5e387f421cd03ccdaee35228b160e9d0b5a24da2
Tharindu Kalhara <tharindukalhara73@gmail.com>
2024-12-09 21:30:31 +0530

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+// Simple App Example
+const a = 500;
+const b = 200;
+
+const calculateDifference = (num1, num2) => num1 - num2;
+
+console.log(`The Difference of a and b is: ${calculateDifference(a, b)}`);
